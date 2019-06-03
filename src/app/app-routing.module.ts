dc5e40b413ecd09a84be417a52148741acd1ad6f
umlautmatt <mahawley24@gmail.com>
2019-06-03 15:37:51 -0400

    about page update

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,11 +2,12 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { ProductsComponent } from './products/products.component';
 import { AboutComponent } from './about/about.component';
+import { ContactComponent } from './contact/contact.component';
 
 const routes: Routes = [ 
   {path: 'pokemon', component: ProductsComponent},
   {path: 'about', component: AboutComponent},
-  
+  {path: 'contact', component: ContactComponent}
 ];
 
 @NgModule({
