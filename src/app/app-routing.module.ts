6725a0c63e5124f4f77b5eda019ebef48903d14d
zringis <zringis@gmail.com>
2019-06-03 16:39:25 -0400

    Contact Page & CSS

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -8,10 +8,7 @@ const routes: Routes = [
   {path: 'pokemon', component: ProductsComponent},
   {path: 'about', component: AboutComponent},
   {path: 'contact', component: ContactComponent}
-<<<<<<< HEAD
   
-=======
->>>>>>> 48527521e7c4266ec5376e972c66e51b00f70c14
 ];
 
 @NgModule({
